c71d10d0bf1e0a5603d725118ce8c86227c16594
Cayman Gill <cayman.g@hotmail.com>
2021-11-07 13:55:55 -0500

    added typeDefs and start resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const {gql} =require('apollo-server-express');
+
+const typeDefs = gql`
+    tpye Query{
+        me: User
+    }
+
+    type User{
+        _id: ID
+        username: String
+        email: String 
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book{
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth{
+        token: ID!
+        user: User
+    }
+
+    type Mutation{
+        login (email: String!, password: String!): Auth
+        addUser (email: String!, password: String!, username: String!): Auth
+        saveBook (input: [BookInput]): User
+        removeBook (bookId: ID!): User
+    }
+`;
+
+module.exports = typeDefs;
